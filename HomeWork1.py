8a8dd234fe0bb2d37770989f021be4ba532407d9
DylanApps <51179794+DylanApps@users.noreply.github.com>
2019-05-29 20:49:19 -0500

    Add files via upload

---
+++ HomeWork1.py
@@ -0,0 +1,12 @@
+Genre,Artist,ArtistAge,Song,DurationInSeconds,Album,ReleaseDate,Awards,Grammy,NumberOfWords = "Genre:Rap, Hiphop","Artis:Eminem","Age:46","Song:Rap God","Seconds:369","Album:The Marshall Mathers LP 2","Release:October 14 2013","""Awards:MTV Video Music Award for Best Editing
+-Most Words in a Hit Single(Guinness World Records)""","Grammy Nominee: Yes","Words:1,560"
+print(Artist)
+print(ArtistAge)
+print(Song)
+print(Genre)
+print(DurationInSeconds)
+print(Album)
+print(ReleaseDate)
+print(Awards)
+print(Grammy)
+print(NumberOfWords)
